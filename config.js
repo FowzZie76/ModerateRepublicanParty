8968e6daf3ddaba436a3481fb56b0ef55fbc41e1
FowzZie <89039949+FowzZie76@users.noreply.github.com>
2025-10-09 20:33:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const adherents = [
     commentaire: "Aujourd’hui, je ressens une grande fierté en annonçant le lancement officiel du Moderate Republican Party (MRP), fruit d’un travail collectif acharné et de la passion de toute l’équipe qui compose notre parti. Depuis des mois, nous avons réfléchi à la manière dont nous pourrions représenter au mieux les valeurs qui nous sont chères tout en répondant aux besoins concrets de notre communauté. Le MRP incarne ainsi nos convictions profondes, qu’il s’agisse de responsabilité, de respect des citoyens et de justice sociale, mais aussi notre engagement à relever les défis actuels auxquels chaque habitant de San Andreas est confronté au quotidien.\n \n Il est essentiel pour nous que chaque membre de notre communauté comprenne l’importance de son rôle dans cette démarche collective. Chaque action, qu’elle soit petite ou grande, a un impact et contribue à façonner un avenir meilleur pour notre région. Nous encourageons chaque citoyen à s’impliquer activement, que ce soit par le dialogue, le bénévolat ou la participation aux initiatives locales, car la force de notre parti repose sur l’engagement concret et sincère de chacun.\n \nPlus que jamais, nous devons travailler ensemble, échanger des idées, soutenir les projets qui ont du sens et défendre les principes qui font de notre société un lieu où chacun peut s’épanouir. Aujourd’hui, le lancement du MRP est non seulement un symbole de nos aspirations, mais également un appel à l’action pour tous ceux qui partagent notre vision et souhaitent contribuer à un San Andreas plus dynamique, solidaire et inclusif. Ensemble, nous pouvons transformer nos convictions en actions concrètes et construire un futur dont chaque citoyen pourra être fier.",
     photo: "https://files.catbox.moe/z38rnq.png",
     lien: "https://life.introrp.com/RyanCole?ref=qs",
-    couleur: "diamond"
+    couleur: "gris"
   },
   {
     nom: "Ethan Carter",
@@ -81,3 +81,4 @@ const adherents = [
   },
 
 ];
+
