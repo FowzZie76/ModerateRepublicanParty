4ad782d0948c981190acad82649902c8bcc91b72
FowzZie <89039949+FowzZie76@users.noreply.github.com>
2025-10-09 02:40:42 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+const adherents = [
+  {
+    nom: "Ryan Cole",
+    age: 30,
+    date: "15/08/2025",
+    profession: "Président du Moderate Republican Party",
+    commentaire: "Aujourd’hui, je ressens une grande fierté en annonçant le lancement officiel du Moderate Republican Party (MRP), fruit d’un travail collectif acharné et de la passion de toute l’équipe qui compose notre parti. Depuis des mois, nous avons réfléchi à la manière dont nous pourrions représenter au mieux les valeurs qui nous sont chères tout en répondant aux besoins concrets de notre communauté. Le MRP incarne ainsi nos convictions profondes, qu’il s’agisse de responsabilité, de respect des citoyens et de justice sociale, mais aussi notre engagement à relever les défis actuels auxquels chaque habitant de San Andreas est confronté au quotidien.\n \n Il est essentiel pour nous que chaque membre de notre communauté comprenne l’importance de son rôle dans cette démarche collective. Chaque action, qu’elle soit petite ou grande, a un impact et contribue à façonner un avenir meilleur pour notre région. Nous encourageons chaque citoyen à s’impliquer activement, que ce soit par le dialogue, le bénévolat ou la participation aux initiatives locales, car la force de notre parti repose sur l’engagement concret et sincère de chacun.\n \nPlus que jamais, nous devons travailler ensemble, échanger des idées, soutenir les projets qui ont du sens et défendre les principes qui font de notre société un lieu où chacun peut s’épanouir. Aujourd’hui, le lancement du MRP est non seulement un symbole de nos aspirations, mais également un appel à l’action pour tous ceux qui partagent notre vision et souhaitent contribuer à un San Andreas plus dynamique, solidaire et inclusif. Ensemble, nous pouvons transformer nos convictions en actions concrètes et construire un futur dont chaque citoyen pourra être fier.",
+    photo: "https://files.catbox.moe/z38rnq.png",
+    lien: "https://life.introrp.com/RyanCole?ref=qs",
+    couleur: "diamond"
+  },
+  {
+    nom: "Ethan Carter",
+    age: 31,
+    date: "15/08/2025",
+    profession: "Vice-président du Moderate Republican Party",
+    commentaire: "Aujourd’hui, je suis fier d’avoir créé, avec tous les collaborateurs du parti, le Moderate Republican Party, qui reflète parfaitement mon état d’esprit et, surtout, les besoins actuels de notre État. Il est essentiel que chaque citoyen de San Andreas s’implique en politique, car à l’heure actuelle, chaque vote compte.",
+    photo: "https://files.catbox.moe/nd8nql.png",
+    lien: "https://life.introrp.com/Ethan_Carter94",
+    couleur: "rouge"
+  },
+  {
+    nom: "████████ ████████████",
+    age: 20,
+    date: "10/09/2025",
+    profession: "Deputy LSCSD | Master FTO",
+    commentaire: "Je ne voulais pas exprimer de commentaire particulier.",
+    photo: "https://files.catbox.moe/a2s966.png",
+    lien: "https://life.introrp.com/",
+    couleur: "doré"
+  },
+  {
+    nom: "██████████ ██████",
+    age: 16,
+    date: "10/09/2025",
+    profession: "Journaliste | CNT",
+    commentaire: "La liberté et le droit de s’informer sont essentiels, surtout pour les journalistes. C’est pourquoi j’adhère aux idées du Moderate Republican Party.",
+    photo: "https://files.catbox.moe/a2s966.png",
+    lien: "https://life.introrp.com/",
+    couleur: "gris"
+  },
+];
