85052f687a2cb86184dd349a116e9ab0693f1c7c
FowzZie <89039949+FowzZie76@users.noreply.github.com>
2025-10-09 20:38:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const adherents = [
     date: "15/08/2025",
     profession: "Président du Moderate Republican Party",
     commentaire: "Aujourd’hui, je ressens une grande fierté en annonçant le lancement officiel du Moderate Republican Party (MRP), fruit d’un travail collectif acharné et de la passion de toute l’équipe qui compose notre parti. Depuis des mois, nous avons réfléchi à la manière dont nous pourrions représenter au mieux les valeurs qui nous sont chères tout en répondant aux besoins concrets de notre communauté. Le MRP incarne ainsi nos convictions profondes, qu’il s’agisse de responsabilité, de respect des citoyens et de justice sociale, mais aussi notre engagement à relever les défis actuels auxquels chaque habitant de San Andreas est confronté au quotidien.\n \n Il est essentiel pour nous que chaque membre de notre communauté comprenne l’importance de son rôle dans cette démarche collective. Chaque action, qu’elle soit petite ou grande, a un impact et contribue à façonner un avenir meilleur pour notre région. Nous encourageons chaque citoyen à s’impliquer activement, que ce soit par le dialogue, le bénévolat ou la participation aux initiatives locales, car la force de notre parti repose sur l’engagement concret et sincère de chacun.\n \nPlus que jamais, nous devons travailler ensemble, échanger des idées, soutenir les projets qui ont du sens et défendre les principes qui font de notre société un lieu où chacun peut s’épanouir. Aujourd’hui, le lancement du MRP est non seulement un symbole de nos aspirations, mais également un appel à l’action pour tous ceux qui partagent notre vision et souhaitent contribuer à un San Andreas plus dynamique, solidaire et inclusif. Ensemble, nous pouvons transformer nos convictions en actions concrètes et construire un futur dont chaque citoyen pourra être fier.",
-    photo: "https://files.catbox.moe/z38rnq.png",
+    photo: "/assets/RyanCole.png",
     lien: "https://life.introrp.com/RyanCole?ref=qs",
-    couleur: "gris"
+    couleur: "diamond"
   },
   {
     nom: "Ethan Carter",
@@ -15,17 +15,17 @@ const adherents = [
     date: "15/08/2025",
     profession: "Vice-président du Moderate Republican Party",
     commentaire: "Aujourd’hui, je suis fier d’avoir créé, avec tous les collaborateurs du parti, le Moderate Republican Party, qui reflète parfaitement mon état d’esprit et, surtout, les besoins actuels de notre État. Il est essentiel que chaque citoyen de San Andreas s’implique en politique, car à l’heure actuelle, chaque vote compte.",
-    photo: "https://files.catbox.moe/nd8nql.png",
+    photo: "/assets/EthanCarter.png",
     lien: "https://life.introrp.com/Ethan_Carter94",
     couleur: "diamond"
   },
   {
-    nom: "Aider Dellair",
+    nom: "Ader Dellair",
     age: 28,
     date: "15/08/2025",
     profession: "Secrétaire général",
     commentaire: "Aujourd’hui, je mesure le chemin parcouru. Ensemble, avec les membres du parti, nous avons posé les bases d’un mouvement qui place les citoyens au cœur de la décision. Le Moderate Republican Party n’est pas seulement une organisation politique, c’est une vision pour une San Andreas plus juste et plus unie.",
-    photo: "https://files.catbox.moe/qp2p0p.png",
+    photo: "/assets/AdenDellair.png",
     lien: "https://life.introrp.com/Magroz?ref=qs",
     couleur: "diamond"
   },
@@ -35,7 +35,7 @@ const adherents = [
     date: "18/08/2025",
     profession: "Secrétaire à l’information",
     commentaire: "Notre objectif a toujours été clair : redonner la parole à ceux qui la méritent, les habitants de San Andreas. Grâce à une équipe dévouée, nous avançons chaque jour vers une politique fondée sur l’écoute, la transparence et la responsabilité.",
-    photo: "https://files.catbox.moe/uby4ez.png",
+    photo: "/assets/HollowayReagan.png",
     lien: "https://life.introrp.com/R.Holloway?ref=qs",
     couleur: "diamond"
   },
@@ -45,7 +45,7 @@ const adherents = [
     date: "20/08/2025",
     profession: "Vice-secrétaire à l’information",
     commentaire: "Je suis profondément reconnaissant envers tous ceux qui ont cru en notre projet. Le Moderate Republican Party représente bien plus qu’un simple parti : c’est un engagement collectif pour un avenir stable, équilibré et prospère.",
-    photo: "https://files.catbox.moe/drhf9p.png",
+    photo: "/assets/AustinDeSullivan.png",
     lien: "https://life.introrp.com/ModerateSullivan?ref=qs",
     couleur: "diamond"
   },
@@ -55,7 +55,7 @@ const adherents = [
     date: "18/08/2025",
     profession: "Ancien agent du FIB",
     commentaire: "Je suis fière aujourd’hui d’avoir adhéré au Moderate Republican Party. C’est une décision mûrement réfléchie, guidée par mon envie de m’impliquer activement dans la vie politique de San Andreas. Je crois profondément que chaque voix peut faire la différence, et je veux que la mienne serve à construire un avenir plus juste et équilibré pour tous.",
-    photo: "https://files.catbox.moe/a2s966.png",
+    photo: "/assets/Anonym.png",
     lien: "https://life.introrp.com/",
     couleur: "doré"
   },
@@ -65,7 +65,7 @@ const adherents = [
     date: "18/08/2025",
     profession: "Ancien agent du PSU",
     commentaire: "Aujourd’hui, je suis fière de rejoindre le Moderate Republican Party. Ce choix représente pour moi bien plus qu’un simple engagement politique : c’est un acte de conviction, un pas concret vers une San Andreas plus forte, plus unie et plus à l’écoute de ses citoyens. Ensemble, nous pouvons transformer nos idées en actions.",
-    photo: "https://files.catbox.moe/a2s966.png",
+    photo: "/assets/Anonym.png",
     lien: "https://life.introrp.com/",
     couleur: "doré"
   },
@@ -75,10 +75,11 @@ const adherents = [
     date: "18/08/2025",
     profession: "Ancien agent du USMS",
     commentaire: "C’est avec une grande fierté que j’annonce mon adhésion au Moderate Republican Party. J’ai choisi de m’engager aux côtés d’hommes et de femmes qui partagent mes valeurs de respect, de modération et de progrès. San Andreas mérite une politique responsable, fondée sur le dialogue et le bon sens — et je suis prête à y contribuer pleinement.",
-    photo: "https://files.catbox.moe/a2s966.png",
+    photo: "/assets/Anonym.png",
     lien: "https://life.introrp.com/",
     couleur: "doré"
   },
 
 ];
+
 
